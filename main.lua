477013fcccd435a24aa0f76cbae411e9704fe378
learnhtsd <160567527+learnhtsd@users.noreply.github.com>
2026-05-03 12:40:13 -0400

    Update version from v12 to v13 in main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local User = "learnhtsd"
 local Repo = "DynxeLT2"
 local Branch = "main"
-local Version = "v12 - dev branch"
+local Version = "v13 - dev branch"
 
 task.spawn(function()
     local ICON_FOLDER  = "DynxeLT2"
