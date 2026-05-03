29c1fd8852a2948d9090a1e598baacc268eb284f
learnhtsd <160567527+learnhtsd@users.noreply.github.com>
2026-05-03 12:09:47 -0400

    Update version from v8 to v9 in main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local User = "learnhtsd"
 local Repo = "DynxeLT2"
 local Branch = "main"
-local Version = "v8 - dev branch"
+local Version = "v9 - dev branch"
 
 task.spawn(function()
     local ICON_FOLDER  = "DynxeLT2"
